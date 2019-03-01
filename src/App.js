a95b376148410f589761f08de5bf0ed2d4ef70ff
carlos114771 <carlosalfredoriveramilian@yahoo.es>
2019-03-01 11:46:42 -0600

    probando

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Componentes/NavigationBar';
 import Footer from './Componentes/Footer';
-import Nav from './Componentes/NavBar';
 import Login from './Componentes/Login'
 class App extends Component {
   render() {
